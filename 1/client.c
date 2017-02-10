50f8a3e0662ce4da448f321b1a330a9d166817c2
vstrimaitis <vstrimaitis@gmail.com>
2017-02-10 19:01:41 +0200

    Multiclient support

--- 1/client.c
+++ 1/client.c
@@ -7,16 +7,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFLEN 256
+#define BUFFLEN 1024
 
 void playGame(int socket){
     char buffer[BUFFLEN];
+    memset(&buffer, 0, sizeof(buffer));
+    recv(socket, buffer, BUFFLEN, 0);
+    fprintf(stdout, "%s\n", buffer);
+    fprintf(stdout, "Enter any character to begin: ");
+    char c = getchar();
+    send(socket, "start", 6, 0);
     while(1){
         memset(&buffer, 0, sizeof(buffer));
         recv(socket,buffer,BUFFLEN,0);
         if(strlen(buffer) == 0)
             break;
-        printf("%s", buffer);
+        fprintf(stdout, "%s", buffer);
 
         send(socket, "ping", 5, 0);
         int ping = recv(socket, buffer, BUFFLEN, 0);
@@ -25,12 +31,11 @@ void playGame(int socket){
 
         memset(&buffer, 0, sizeof(buffer));
         scanf("%s", buffer);
-        //printf("Sending %s (%d)\n", buffer, (int)strlen(buffer));
         send(socket,buffer,strlen(buffer),0);
     }
 }
 
-int main(int argc, char *argv[]){   
+int main(int argc, char *argv[]){
     unsigned int port;
     int s_socket;
     struct sockaddr_in servaddr; // Serverio adreso struktûra
@@ -38,14 +43,14 @@ int main(int argc, char *argv[]){
     char buffer[BUFFLEN];
 
     if (argc != 3){
-        fprintf(stderr,"USAGE: %s <ip> <port>\n",argv[0]);
+        fprintf(stdout,"USAGE: %s <ip> <port>\n",argv[0]);
         exit(1);
     }
 
     port = atoi(argv[2]);
 
     if ((port < 1) || (port > 65535)){
-        printf("ERROR #1: invalid port specified.\n");
+        fprintf(stderr, "ERROR #1: invalid port specified.\n");
         exit(1);
     }
 
@@ -81,19 +86,8 @@ int main(int argc, char *argv[]){
         fprintf(stderr,"ERROR #4: error in connect().\n");
         exit(1);
     }
-    /*
-    printf("Enter the message: ");
-    fgets(buffer, BUFFLEN, stdin);
-    send(s_socket,buffer,strlen(buffer),0);
-
-    memset(&buffer,0,BUFFLEN);
-    recv(s_socket,buffer,BUFFLEN,0);
-    printf("Server sent: %s\n", buffer);
-*/
+  
     playGame(s_socket);
-    /*
-     * Socket'as uþdaromas
-     */
     close(s_socket);
     return 0;
 }
--- 1/server.c
+++ 1/server.c
@@ -6,74 +6,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFF_LEN 256
+#include <math.h>
+#define BUFF_LEN 1024
+#define MAX_CLIENTS 10
 
 const int MIN_VAL = 1;
 const int MAX_VAL = 100;
 
- typedef enum {
-	Correct,
-	Less,
-	More,
-	Invalid
+typedef enum {
+    Correct,
+    Less,
+    More,
+    Invalid
 } Answer;
+
+typedef struct{
+    struct sockaddr_in address;
+    int socket;
+    int threadId;
+} ClientInfo;
+
+pthread_t threads[MAX_CLIENTS];
 
 Answer getInput(struct sockaddr_in clientaddr, int socket){
     char buffer[BUFF_LEN];
     memset(&buffer, 0, sizeof(buffer));
     int s_len = recv(socket,buffer,sizeof(buffer),0);
-    printf("IP: %s\tReceived: '%s'\n",inet_ntoa(clientaddr.sin_addr), buffer);
+    fprintf(stdout, "IP: %s\tReceived: '%s'\n",inet_ntoa(clientaddr.sin_addr), buffer);
     int i = 0;
     for(; i < strlen(buffer); i++)
-    	buffer[i] = tolower(buffer[i]);
+        buffer[i] = tolower(buffer[i]);
     if(strcmp(buffer, "yes") == 0)
-    	return Correct;
+        return Correct;
     if(strcmp(buffer, "less") == 0)
-    	return Less;
+        return Less;
     if(strcmp(buffer, "more") == 0)
-    	return More;
+        return More;
     return Invalid;
 }
 
-void playGame(struct sockaddr_in clientaddr, int socket, int min, int max){
-	int lo = min, hi = max, guesses = 1;
-	char buffer[BUFF_LEN];
-	while(1){
-		if(hi < lo){
-			printf("You lied to me ;(\n");
-			break;
-		}
-		int mid = (hi+lo)/2;
-		memset(&buffer, 0, sizeof(buffer));
-		sprintf(buffer, "%d) Is your number %d? (Yes/Less/More) ", guesses, mid);
-		printf("IP: %s\tGuessing: %d\n", inet_ntoa(clientaddr.sin_addr), mid);
-		send(socket,buffer,strlen(buffer),0);
-		while(1){
-			Answer ans = getInput(clientaddr, socket);
-			if(ans == Correct){
-				//printf("Gotcha in %d guesses ;)\n", guesses);
-				printf("IP: %s\tWon in %d guesses\n", inet_ntoa(clientaddr.sin_addr), guesses);
-				sprintf(buffer, "Gotcha in %d guesses. Good game ;)\n", guesses);
-				send(socket,buffer,strlen(buffer),0);
-				return;
-			} else if(ans == Less){
-				hi = mid-1;
-				break;
-			} else if(ans == More){
-				lo = mid+1;
-				break;
-			} else{
-				//printf("Invalid choice!\n");
-				sprintf(buffer, "Invalid choice!\n");
-				send(socket,buffer,strlen(buffer),0);
-			}
-		}
-		guesses++;
-	}
+void playGame(struct sockaddr_in clientaddr, int socket){
+    int lo = MIN_VAL, hi = MAX_VAL, guesses = 1;
+    char buffer[BUFF_LEN];
+    int maxGuesses = ceil(log(MAX_VAL - MIN_VAL + 2) / log(2));
+    sprintf(buffer, "%s%s%d%s%d%s%s%d%s%s%s%s",
+        "==============================================================\n",
+        "Think of a number between ", lo, " and ", hi," (inclusive).\n",
+        "I'll try to guess it in not more than ", maxGuesses, " tries.\n",
+        "I'll keep guessing and you'll have to answer, whether\n",
+        "your number is larger or smaller than the one I'm guessing.\n",
+        "==============================================================\n");
+    send(socket, buffer, strlen(buffer), 0);
+    getInput(clientaddr, socket);
+    while(1){
+        if(hi < lo){
+            sprintf(buffer, "You lied to me ;(\n");
+            send(socket, buffer, strlen(buffer), 0);
+            break;
+        }
+        int mid = (hi+lo)/2;
+        memset(&buffer, 0, sizeof(buffer));
+        sprintf(buffer, "%d) Is your number %d? (Yes/Less/More) ", guesses, mid);
+        fprintf(stdout, "IP: %s\tGuessing: %d\n", inet_ntoa(clientaddr.sin_addr), mid);
+        send(socket,buffer,strlen(buffer),0);
+        while(1){
+            Answer ans = getInput(clientaddr, socket);
+            if(ans == Correct){
+                fprintf(stdout, "IP: %s\tWon in %d guesses\n", inet_ntoa(clientaddr.sin_addr), guesses);
+                sprintf(buffer, "Gotcha in %d guesses. Good game ;)\n", guesses);
+                send(socket,buffer,strlen(buffer),0);
+                return;
+            } else if(ans == Less){
+                hi = mid-1;
+                break;
+            } else if(ans == More){
+                lo = mid+1;
+                break;
+            } else{
+                sprintf(buffer, "Invalid choice!\n");
+                send(socket,buffer,strlen(buffer),0);
+            }
+        }
+        guesses++;
+    }
+}
+
+void* runNewGameInstance(void *client){
+    ClientInfo ci = *(ClientInfo*)client;
+    playGame(ci.address, ci.socket);
+    fprintf(stdout, "Game finished for %s.\n", inet_ntoa(ci.address.sin_addr));
+    close(ci.socket);
+    threads[ci.threadId] = -1;
+    pthread_exit(0);
+}
+
+int getEmptySlotIndex(pthread_t* slots){
+    int i;
+    for(i = 0; i < MAX_CLIENTS; i++)
+        if(slots[i] == -1)
+            return i;
+    return -1;
 }
 
 int main(int argc, char *argv[]){
-	unsigned int port;
+    unsigned int port;
     int l_socket; // socket'as skirtas prisijungim� laukimui
     int c_socket; // prisijungusio kliento socket'as
 
@@ -85,16 +121,20 @@ int main(int argc, char *argv[]){
     int s_len;
     int r_len;
     char buffer[BUFF_LEN];
+
+    int i;
+    for(i = 0; i < MAX_CLIENTS; i++)
+        threads[i] = -1;
     
     if (argc != 2){
-        printf("USAGE: %s <port>\n", argv[0]);
+        fprintf(stdout, "USAGE: %s <port>\n", argv[0]);
         exit(1);
     }
 
     port = atoi(argv[1]);
 
     if ((port < 1) || (port > 65535)){
-        printf("ERROR #1: invalid port specified.\n");
+        fprintf(stderr, "ERROR #1: invalid port specified.\n");
         exit(1);
     }
 
@@ -151,12 +191,21 @@ int main(int argc, char *argv[]){
             (struct sockaddr*)&clientaddr,&clientaddrlen))<0){
             fprintf(stderr,"ERROR #5: error occured accepting connection.\n");
             exit(1);
+        } else{
+            int index = getEmptySlotIndex(threads);
+            if(index == -1){
+                sprintf(buffer, "No more available slots :/");
+                send(c_socket, buffer, strlen(buffer), 0);
+            } else {
+                ClientInfo ci;
+                ci.address = clientaddr;
+                ci.socket = c_socket;
+                ci.threadId = index;
+                pthread_create(&threads[index], NULL, runNewGameInstance, (void*)&ci);
+                threads[i] = 0;
+            }
         }
-
-        playGame(clientaddr, c_socket, MIN_VAL, MAX_VAL);
-        printf("Game finished.\n");
-        close(c_socket);
-        printf("Socket closed.\n");
+        
     }
 
     return 0;
