c48f3a968ffe09e4ec5ae27a3e57b6cffae6db65
vstrimaitis <vstrimaitis@gmail.com>
2017-03-14 14:53:06 +0200

    Added pure message parsing from the server's response.

--- 2/SmtpClient/Mail/Response.cs
+++ 2/SmtpClient/Mail/Response.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Mail
 {
@@ -6,11 +8,15 @@ namespace Mail
     {
         public int Code { get; private set; }
         public IEnumerable<string> Message { get; private set; }
+        public IEnumerable<string> ShortMessage { get; private set; } // The original message except with no reply or status code
 
         public Response(int code, IEnumerable<string> message)
         {
             Code = code;
             Message = message;
+            ShortMessage = message.Select(x => x.Substring(4, x.Length - 4))
+                                  .Select(x => Regex.IsMatch(x, @"\d+\.\d+\.\d+.*") ? string.Join("", Regex.Split(x, @"\d+\.\d+\.\d+").Skip(1)).Trim()
+                                                                                    : x);
         }
 
         public override string ToString()
