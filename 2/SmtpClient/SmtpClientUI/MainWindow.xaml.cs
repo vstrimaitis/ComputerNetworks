c41fc7f8783e44aae23dcfbb92b0a3c779fd6ff3
vstrimaitis <vstrimaitis@gmail.com>
2017-03-19 14:44:46 +0200

    Cleaned up the using statements.

--- 2/SmtpClient/SmtpClientUI/MainWindow.xaml.cs
+++ 2/SmtpClient/SmtpClientUI/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
-﻿using Logging;
-using Mail;
+﻿using Mail;
 using Mail.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 namespace SmtpClientUI
