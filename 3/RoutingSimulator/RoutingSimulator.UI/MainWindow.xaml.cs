32b231a9add749b9c479e71d66d43f8c72290098
vstrimaitis <vstrimaitis@gmail.com>
2017-03-31 12:37:37 +0300

    Added circles.

--- 3/RoutingSimulator/RoutingSimulator.UI/MainWindow.xaml.cs
+++ 3/RoutingSimulator/RoutingSimulator.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoutingSimulator.UI.Shapes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,63 @@ namespace RoutingSimulator.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Circle _previousCircle;
+        private Circle _hoveringOver;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = e.GetPosition(canvas);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if(_hoveringOver != null)
+                {
+                    _hoveringOver.Position = new Point(pos.X - _hoveringOver.Radius, pos.Y - _hoveringOver.Radius);
+                }
+                else
+                {
+                    if (_previousCircle == null)
+                    {
+                        _previousCircle = new Circle(20, fill: new SolidColorBrush(Color.FromArgb(100, 255, 255, 255)),
+                                                         stroke: new SolidColorBrush(Color.FromArgb(100, 0, 0, 0)),
+                                                         labelColor: new SolidColorBrush(Color.FromArgb(100, 0, 0, 0)));
+                        _previousCircle.MouseEnter += (s, args) =>
+                        {
+                            this.Cursor = Cursors.Hand;
+                            _hoveringOver = s as Circle;
+                        };
+                        _previousCircle.MouseLeave += (s, args) =>
+                        {
+                            this.Cursor = Cursors.Arrow;
+                            _hoveringOver = null;
+                        };
+                        foreach (var el in _previousCircle.UIElements)
+                        {
+                            canvas.Children.Add(el);
+                        
+                        }
+                    }
+                    _previousCircle.Position = new Point(pos.X - _previousCircle.Radius, pos.Y - _previousCircle.Radius);
+                }
+            }
+            else if (_previousCircle != null)
+            {
+                _previousCircle = null;
+            }
+        }
+
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_previousCircle == null)
+                return;
+            _previousCircle.Fill = new SolidColorBrush(Colors.White);
+            _previousCircle.Stroke = new SolidColorBrush(Colors.Black);
+            _previousCircle.LabelColor = new SolidColorBrush(Colors.Black);
+            _previousCircle = null;
         }
     }
 }
---
+++ 3/RoutingSimulator/RoutingSimulator.UI/Shapes/Circle.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace RoutingSimulator.UI.Shapes
+{
+    public class Circle
+    {
+        private class CircleLabel
+        {
+            private Label _label;
+            public Label UILabel
+            {
+                get
+                {
+                    return _label;
+                }
+            }
+            public string Text
+            {
+                get
+                {
+                    return _label.Content.ToString();
+                }
+                set
+                {
+                    _label.Content = value;
+                }
+            }
+            public Brush Foreground
+            {
+                get
+                {
+                    return _label.Foreground;
+                }
+                set
+                {
+                    _label.Foreground = value;
+                }
+            }
+            public Brush Background
+            {
+                get
+                {
+                    return _label.Background;
+                }
+                set
+                {
+                    _label.Background = value;
+                }
+            }
+            public double Height
+            {
+                get
+                {
+                    _label.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                    return _label.DesiredSize.Height;
+                }
+            }
+            public double Width
+            {
+                get
+                {
+                    _label.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                    return _label.DesiredSize.Width;
+                }
+            }
+
+            public CircleLabel() : this(new SolidColorBrush(Colors.Black), null)
+            { }
+
+            public CircleLabel(Brush foreground, Brush background)
+            {
+                _label = new Label();
+                Foreground = foreground;
+                Background = background;
+                Text = Id;
+            }
+
+            private static int _id = 0;
+            private const string Letters = "abcdefghijklmnopqrstuvwxyz";
+            private static string Id
+            {
+                get
+                {
+                    string result = "";
+                    int b = Letters.Length;
+                    int tmpId = _id++;
+                    while(tmpId >= 0)
+                    {
+                        result = Letters[tmpId % b] + result;
+                        tmpId /= b;
+                        tmpId--;
+                    }
+                    return result;
+                }
+            }
+        }
+        private Ellipse _circle;
+        private CircleLabel _label;
+        private Point _position;
+
+        public Brush Fill
+        {
+            get
+            {
+                return _circle.Fill;
+            }
+            set
+            {
+                _circle.Fill = value;
+            }
+        }
+        public Brush Stroke
+        {
+            get
+            {
+                return _circle.Stroke;
+            }
+            set
+            {
+                _circle.Stroke = value;
+            }
+        }
+        public Brush LabelColor
+        {
+            get
+            {
+                return _label.Foreground;
+            }
+            set
+            {
+                _label.Foreground = value;
+            }
+        }
+        public double Radius
+        {
+            get
+            {
+                return _circle.Width / 2;
+            }
+            set
+            {
+                _circle.Width = _circle.Height = 2 * value;
+            }
+        }
+        public Point Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+                Canvas.SetTop(_circle, _position.Y);
+                Canvas.SetLeft(_circle, _position.X);
+                Canvas.SetTop(_label.UILabel, _position.Y + _circle.Height / 2 - _label.Height / 2);
+                Canvas.SetLeft(_label.UILabel, _position.X + _circle.Width / 2 - _label.Width / 2);
+            }
+        }
+        public IEnumerable<UIElement> UIElements
+        {
+            get
+            {
+                yield return _circle;
+                yield return _label.UILabel;
+            }
+        }
+
+        public Circle() : this(1, new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Black), new SolidColorBrush(Colors.Black))
+        { }
+
+        public Circle(double radius) : this(radius, new SolidColorBrush(Colors.White), new SolidColorBrush(Colors.Black), new SolidColorBrush(Colors.Black))
+        { }
+
+        public Circle(double radius, Brush fill, Brush stroke, Brush labelColor)
+        {
+            _circle = new Ellipse();
+            Radius = radius;
+            Fill = fill;
+            Stroke = stroke;
+            _label = new CircleLabel();
+            Position = new Point(0, 0);
+            _circle.MouseEnter += (s, e) => MouseEnter?.Invoke(this, e);
+            _circle.MouseLeave += (s, e) => MouseLeave?.Invoke(this, e);
+            _label.UILabel.MouseEnter += (s, e) => MouseEnter?.Invoke(this, e);
+            _label.UILabel.MouseLeave += (s, e) => MouseLeave?.Invoke(this, e);
+        }
+
+
+        public event EventHandler<MouseEventArgs> MouseEnter;
+        public event EventHandler<MouseEventArgs> MouseLeave;
+    }
+}
